3c59b61a14dddca5ae393f469ee62fd71363b4ed
QuocanhKieu <44871775+QuocanhKieu@users.noreply.github.com>
2023-05-23 09:27:12 +0700

    Add files via upload

---
+++ doWhile.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<conio.h>
+#include <string.h>
+
+int main() {
+	int num = 0;
+	
+	do {
+		printf("%d\n",num);
+		num++;
+	}while(num<=5);
+
+
+	return 0;
+}
+
---
+++ while loop.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<conio.h>
+#include <string.h>
+
+int main() {
+	int num = 0;
+	
+	while(num <=5) {
+		printf("%d\n",num);
+		num++;
+	}
+
+
+	return 0;
+}
+
